9c0ad34089e568b6f9eeacc3a1c6f01bc675b7dd
Ran Tavory <rantav@gmail.com>
2014-02-18 11:02:58 +0200

    Add a blueprint for tty.js

---
+++ tty.js/start.sh
@@ -0,0 +1,34 @@
+set -e
+
+pwd
+env
+
+echo Installing tty.js
+
+sudo apt-get update
+sudo apt-get install git-core curl build-essential openssl libssl-dev -y
+cd /tmp/
+
+# Node
+git clone https://github.com/joyent/node.git
+pushd node
+git checkout v0.10.25
+./configure
+make
+sudo make install
+popd
+
+node -v
+
+# Npm
+git clone git://github.com/isaacs/npm.git
+pushd npm
+sudo make install
+popd
+
+npm -v
+
+# tty.js
+sudo npm -g install tty.js
+
+tty.js --daemonize --port 8080
